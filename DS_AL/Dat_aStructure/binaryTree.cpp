7a08f5a792f2e52e76cfe06149c4c33150333f99
KCheolHyeon <ch970314@naver.com>
2024-03-03 18:38:45 +0900

    이진트리 만들기

---
+++ DS_AL/Dat_aStructure/binaryTree.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+
+typedef int BTData;
+
+typedef struct _bTreeNode
+{
+	BTData data;
+	struct _bTreeNode* left;
+	struct _bTreeNode* right;
+} BTreeNode;
+
+/*** BTreeNode ���� ����� ****/
+BTreeNode* MakeBTreeNode(void) 
+{
+	BTreeNode* nd = (BTreeNode*)malloc(sizeof(BTreeNode));
+	nd->left = NULL;
+	nd->right = NULL;
+	return nd;
+}
+
+BTData GetData(BTreeNode* bt) 
+{
+	return bt->data;
+}
+
+void SetData(BTreeNode* bt, BTData data) 
+{
+	bt->data = data;
+}
+
+BTreeNode* GetLeftSubTree(BTreeNode* bt) 
+{
+	if (bt->left == nullptr) 
+	{
+		return;//오류
+	}
+	return bt->left;
+}
+BTreeNode* GetRightSubTree(BTreeNode* bt) 
+{
+	if (bt->right == nullptr)
+	{
+		return;//오류
+	}
+	return bt->right;
+}
+
+void MakeLeftSubTree(BTreeNode* main, BTreeNode* sub) 
+{
+	if (main->left != NULL)
+		free(main->left);
+
+	main->left = sub;
+}
+void MakeRightSubTree(BTreeNode* main, BTreeNode* sub) 
+{
+	if (main->right != NULL)
+		free(main->right);
+
+	main->right = sub;
+}
